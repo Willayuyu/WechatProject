050f48cac8335a956042a973496b40c429487ef2
Willayuyu <1394361375@qq.com>
2022-04-15 02:39:19 +0800

    云托管

--- miniprogramme/cloud/bookinfo/index.js
+++ miniprogramme/cloud/bookinfo/index.js
@@ -4,10 +4,23 @@ const cloud = require('wx-server-sdk')
 cloud.init()
 
 // 云函数入口函数
+var rp = require('request-promise')
+
 exports.main = async (event, context) => {
     // const wxContext = cloud.getWXContext()
-    console.logI(event);
-    return event.isbn
+    console.log(event);
+    // return event.isbn
+    // const res = await wx.cloud.callContainer({
+    //     path: '', // 填入容器的访问路径（云托管-服务列表-路径）
+    //     method: 'GET',
+    // })
+    // console.log(res)
+    var res = rp('https://book-1826000-1310697903.ap-shanghai.run.tcloudbase.com/isbn/' + event.isbn).then(html => {
+        return html;
+    }).catch(err => {
+        console.log(err)
+    })
+    return res
     // return {
     //     event,
     //     openid: wxContext.OPENID,
