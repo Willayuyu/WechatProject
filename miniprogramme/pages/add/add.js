7c337752c089fdbb381715f6761ce420bea1a6a0
Willayuyu <1394361375@qq.com>
2022-04-20 20:58:20 +0800

    ocr

--- miniprogramme/pages/add/add.js
+++ miniprogramme/pages/add/add.js
@@ -9,6 +9,7 @@ Page({
         select: false,
         plain: true,
         hiddenmodalput: false,
+        words:''
     },
     /**
      * 获取 CapsuleBar 高度
@@ -29,21 +30,46 @@ Page({
         })
     },
 
-    modalinput(){
+    modalinput() {
         this.setData({
             hiddenmodalput: !this.data.hiddenmodalput
         })
     },
-    showExistBook(){
+    showExistBook() {
         wx.navigateTo({
-          url: '/pages/existBooks/existBooks',
+            url: '/pages/existBooks/existBooks',
         })
     },
 
     /**
      * 获取百度access_token
      */
-    onOCR(){
+    onOCR() {
+        var that = this
+        wx.chooseImage({
+            sizeType: ['original', 'compressed'],
+            sourceType: ['album', 'camera'],
+            success: function (res) {
+                const tempFilePath = res.tempFilePaths[0]
+                wx.getFileSystemManager().readFile({
+                    filePath: tempFilePath,
+                    encoding: 'base64',
+                    success: function (res) {
+                        //调用方法
+                        console.log(res)
+                        that.getImgInfo(res.data)
+                    },
+                })
+            }
+        })
+
+    },
+    //根据图片的内容调用API获取图片文字
+    getImgInfo: function (imageData) {
+        wx.showLoading({
+            title: '识别中...',
+        })
+        var that = this
         var APIKEY = "GXKpgeLUVYpqvbtoB65U9s7L"
         var SECKEY = "tAAZOR8BzVH4bKWdqMa7lvYR1zGeCW5e"
         wx.cloud.callFunction({
@@ -51,18 +77,56 @@ Page({
             name: 'getBaiduToken',
             // 传递给云函数的参数
             data: {
-                apikey:APIKEY,
-                seckey:SECKEY
+                apikey: APIKEY,
+                seckey: SECKEY
             },
-            success: res=>{
+            success: res => {
                 console.log(res)
-                
+                //获取token
+                const token = JSON.parse(res.result).access_token
+                console.log(token)
+                const detectUrl = `https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic?access_token=${token}` // baiduToken是已经获取的access_Token 
+                wx.cloud.callFunction({
+                    // 要调用的云函数名称
+                    name: 'getImgInfo',
+                    // 传递给云函数的参数
+                    data: {
+                        url:detectUrl,
+                        image:imageData
+                    },
+                    success:res=>{
+                        console.log(res.result)
+                        //将 res.data.words_result数组中的内容加入到words中
+                        // that.setData({
+                        //     words: JSON.parse(res.result).words_result
+                        // })
+                        console.log(JSON.parse(res.result).words_result)
+                        var word = new Array()
+                        for (let i = 0; i < JSON.parse(res.result).words_result_num; i++){
+                            word.push(JSON.parse(res.result).words_result[i].words)
+                        }
+                        console.log(word)
+                        console.log(word.toString())
+                        console.log(word.toString().replace(/,/g, " "))
+                        that.setData({
+                            words: word.toString().replace(/,/g, " ")
+                        })
+                        wx.hideLoading()
+                    },
+                    fail: function (res, reject) {
+                        console.log('get word fail：', res.result);
+                        wx.hideLoading()
+                    },
+                    complete: function () {
+                        wx.hideLoading()
+                    }
+                })
+
             },
-            fail:err=>{
+            fail: err => {
                 console.error(res)
             }
         })
-
     },
 
     /**
